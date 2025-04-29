52d85f3ee770cdb8ee47cd63f955c9f5e641c65e
JlwxdQueen <jlwxd@mail.ru>
2025-04-29 23:47:49 +0300

    FIX: delete extra fields REFACTOR: run.sh

--- schemas/good/function.sql
+++ schemas/good/function.sql
@@ -1,8 +1,8 @@
 CREATE OR REPLACE FUNCTION add_to_inventory()
 RETURNS TRIGGER AS $$
 BEGIN
-    INSERT INTO inventory_items (item_id, good_id, warehouse_id, shelf, quantity, status, last_updated)
-    VALUES (NEW.item_id, NEW.id, NEW.warehouse_id, 'A1', NEW.quantity, 'available', CURRENT_TIMESTAMP);
+    INSERT INTO inventory_items (item_id, good_id, warehouse_id, quantity, last_updated)
+    VALUES (NEW.item_id, NEW.id, NEW.warehouse_id, NEW.quantity, CURRENT_TIMESTAMP);
 
     -- Добавление записи в stock_transactions
     INSERT INTO stock_transactions (item_id, quantity, transaction_type, warehouse_id, transaction_date)
--- schemas/settings/table.sql
+++ schemas/settings/table.sql
@@ -1,5 +1,6 @@
 CREATE TABLE settings (
     id SERIAL PRIMARY KEY,
+    settings_id INTEGER NOT NULL,
     language language_type NOT NULL,
     time_zone time_zone_type NOT NULL,
     data_format data_format_type NOT NULL,
